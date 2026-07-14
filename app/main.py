be67efe5f25272778b3cabf3e50b70c9ac66c8c4
mikemytony <mikemytony@gmail.com>
2026-07-14 14:33:19 +0100

    Fix: Procfile pour Railway

--- app/main.py
+++ app/main.py
@@ -15,4 +15,10 @@ def home():
         "message": "Welcome to Melodix SmartMix API",
         "status": "online",
         "version": "1.0.0"
+        import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
     }

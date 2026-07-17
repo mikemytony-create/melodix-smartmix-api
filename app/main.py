dd08a1f4710434c6548ab207ac3579718c044c89
mikemytony <mikemytony@gmail.com>
2026-07-17 23:54:47 +0100

    message

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
+import os
 from fastapi import FastAPI
 from app.routes import smartmix, lyrics, translation, audio, cloud, users, health
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+os.makedirs("uploads", exist_ok=True) # <-- MET LE ICI, LIGNE 6
+
 app = FastAPI(title="Melodix API V1")
 app.mount("/static", StaticFiles(directory="uploads"), name="static")
 
+app.include_router(smartmix.router, prefix="/v1/smartmix", tags=["SmartMix"])
+# ... le reste
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 app.include_router(smartmix.router, prefix="/v1/smartmix", tags=["SmartMix"])
 app.include_router(lyrics.router, prefix="/v1/paroles", tags=["Paroles"])
 app.include_router(translation.router, prefix="/v1/traduire", tags=["Traduction"])

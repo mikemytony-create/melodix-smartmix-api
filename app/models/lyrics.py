646a4ea00ff3f1ccbe3072e0407afdd221eb1bc6
mikemytony-create <mikemytony@gmail.com>
2026-07-18 10:05:00 +0100

    Update lyrics.py

--- app/models/lyrics.py
+++ app/models/lyrics.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List
 
+class Song(BaseModel):
+    id: int
+    title: str
+    artist: str
+    album: str
+    genre: str
+    mood: List[str]
+    duration: int # en secondes
+    popularity: float # de 0 à 100
+    release_year: int

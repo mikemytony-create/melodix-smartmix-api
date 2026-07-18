3b9e81c9a702ffcfa476e5512695ecf1b0817d3f
mikemytony <mikemytony@gmail.com>
2026-07-18 15:30:11 +0100

    message

--- app/models/song.py
+++ app/models/song.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Union
 
 class Song(BaseModel):
-    id: str
+    id: Union[str, int] # Accepte "1" ou 1
     title: str
     artist: str
     duration: Optional[float] = None
     url: Optional[str] = None
     genre: Optional[str] = None
-    mood: Optional[str] = None
+    mood: Optional[Union[str, List[str]]] = None # Accepte "happy" ou ["happy", "chill"]
     tempo: Optional[float] = None

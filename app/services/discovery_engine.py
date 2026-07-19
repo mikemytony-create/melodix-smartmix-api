b12bbfcc13104d3b3468a8f4fd1ebddbe5da0cca
mikemytony <mikemytony@gmail.com>
2026-07-19 20:44:18 +0100

    message

--- app/services/discovery_engine.py
+++ app/services/discovery_engine.py
@@ -41,11 +41,86 @@ class DiscoveryEngine:
         for artist in favorite_artists:
 
             if artist in similar:
+                recommendations.extend(similar[artist])
 
-                recommendations.extend(
-                    similar[artist]
-                )
+        return list(set(recommendations))
 
-        return list(
-            set(recommendations)
-        )
+    def discover_similar_genres(
+        self,
+        favorite_genres: List[str]
+    ) -> List[str]:
+
+        similar = {
+
+            "Afrobeats": [
+                "Afropop",
+                "Amapiano",
+                "Dancehall"
+            ],
+
+            "Hip-Hop": [
+                "Trap",
+                "Rap",
+                "R&B"
+            ],
+
+            "Electronic": [
+                "House",
+                "Techno",
+                "Synthwave"
+            ],
+
+            "Pop": [
+                "Dance Pop",
+                "Indie Pop",
+                "Electropop"
+            ]
+
+        }
+
+        recommendations = []
+
+        for genre in favorite_genres:
+
+            if genre in similar:
+                recommendations.extend(similar[genre])
+
+        return list(set(recommendations))
+
+    def discover_new_moods(
+        self,
+        current_mood: str
+    ) -> List[str]:
+
+        moods = {
+
+            "happy": [
+                "party",
+                "energetic",
+                "dance"
+            ],
+
+            "sad": [
+                "calm",
+                "relax",
+                "hope"
+            ],
+
+            "chill": [
+                "focus",
+                "dreamy",
+                "romantic"
+            ],
+
+            "motivation": [
+                "workout",
+                "power",
+                "success"
+            ]
+
+        }
+
+        return moods.get(current_mood.lower(), [])
+
+
+discovery_engine = DiscoveryEngine()
